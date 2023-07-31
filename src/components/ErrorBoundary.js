968645445670ab7c653b501dccfe7e1f642b6022
beescuit <pengsoon20@gmail.com>
2023-08-01 03:59:20 +0900

    create ErrorBoundary component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false,
+    error: null,
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, errorInfo) {}
+
+  handleUnhandled = () => {
+    this.setState({ hasError: true })
+  }
+  componentDidMount() {
+    window.addEventListener('unhandledrejection', this.handleUnhandled)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('unhandledrejection', this.handleUnhandled)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback
+    }
+
+    return this.props.children
+  }
+}
