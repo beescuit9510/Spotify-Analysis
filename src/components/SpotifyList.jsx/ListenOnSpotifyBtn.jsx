69bad79cb1afbbfdfd0d4ac2d2ef85f5f20703a2
beescuit <pengsoon20@gmail.com>
2023-08-07 14:54:28 +0900

    fix: style of ListenOnSpotifyBtn component

--- src/components/SpotifyList.jsx/ListenOnSpotifyBtn.jsx
+++ src/components/SpotifyList.jsx/ListenOnSpotifyBtn.jsx
@@ -11,7 +11,7 @@ export default function ListenOnSpotifyBtn({ spotifyLink }) {
       onClick={() => window.open(spotifyLink)}
     >
       <div className='flex flex-row justify-center items-center leading-3 text-xs font-light sm:text-medium sm:font-normal'>
-        <FaSpotify className='text-2xl' />
+        <FaSpotify className='text-3xl mr-1' />
         <div>
           <span>Listen on </span>
           <span>Spotify</span>
