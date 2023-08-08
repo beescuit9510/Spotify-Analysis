3278308656fc984afc4126f033803b3b416f036f
beescuit <pengsoon20@gmail.com>
2023-08-08 18:52:08 +0900

    refactor: set different ErrorBoundary for Login fail and Content fail.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,15 +2,19 @@ import React, { useEffect } from 'react'
 import SpotifyList from './SpotifyList/index.jsx'
 import Main from './lib/Main.jsx'
 import Me from './Me.jsx'
+import { ErrorBoundary } from './lib/ErrorBoundary.js'
+import Login from './Login.jsx'
 
 function Content() {
   return (
     <>
-      <Me>
-        <Main>
-          <SpotifyList />
-        </Main>
-      </Me>
+      <ErrorBoundary fallback={<Login />}>
+        <Me>
+          <Main>
+            <SpotifyList />
+          </Main>
+        </Me>
+      </ErrorBoundary>
     </>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,12 @@
 import React, { Suspense } from 'react'
 import Header from './Header'
 import { ErrorBoundary } from './lib/ErrorBoundary'
-import Login from './Login'
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <ErrorBoundary fallback={<Login />}>{children}</ErrorBoundary>
+      <ErrorBoundary fallback={<div>ERROR</div>}>{children}</ErrorBoundary>
     </>
   )
 }
