421c8a6d80079af21f698e4c8d277f9edeb8e412
beescuit <pengsoon20@gmail.com>
2023-08-10 03:05:43 +0900

    fix: fix Profile styling, icon height

--- src/components/lib/Profile.jsx
+++ src/components/lib/Profile.jsx
@@ -4,23 +4,24 @@ import { IoPersonCircleSharp } from 'react-icons/io5'
 
 export default function Profile({ src, isLoading = true, handleOnClick }) {
   return (
-    <div>
+    <div className='flex items-center justify-center relative'>
       <Skeleton
         isLoading={isLoading}
         className='aspect-square border rounded-full h-[3.5rem]'
       >
-        {src ? (
-          <img
-            className='aspect-square object-cover rounded-full border shadow-lg cursor-pointer h-full max-h-[3.5rem]'
-            src={src}
-            onClick={handleOnClick}
-          />
-        ) : (
-          <IoPersonCircleSharp
-            className='aspect-square text-gray-300 cursor-pointer h-[3.5rem]'
-            onClick={handleOnClick}
-          />
-        )}
+        <img
+          className={`aspect-square object-cover rounded-full border shadow-lg cursor-pointer h-[3.5rem] ${
+            src ? src : 'invisible'
+          }`}
+          src={src}
+          onClick={handleOnClick}
+        />
+        <IoPersonCircleSharp
+          className={`text-7xl rounded-full text-gray-300 cursor-pointer absolute ${
+            src ? 'invisible' : ''
+          }`}
+          onClick={handleOnClick}
+        />
       </Skeleton>
     </div>
   )
