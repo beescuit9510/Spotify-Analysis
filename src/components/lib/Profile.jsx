240b68c6b4e4890081401beb46f66e1546be7291
beescuit <pengsoon20@gmail.com>
2023-08-09 17:22:54 +0900

    fix: Profile Skeleton style fix

--- src/components/lib/Profile.jsx
+++ src/components/lib/Profile.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Skeleton from './Skeleton'
+import { IoPersonCircleSharp } from 'react-icons/io5'
 
 export default function Profile({ src, isLoading = true, handleOnClick }) {
   return (
     <div>
-      <Skeleton isLoading={isLoading} className='h-[3.5rem] rounded-full'>
-        <img
-          className='aspect-square object-cover rounded-full border shadow-lg cursor-pointer h-full max-h-[3.5rem]'
-          src={src}
-          onClick={handleOnClick}
-        />
+      <Skeleton isLoading={isLoading} className='rounded-full'>
+        {src ? (
+          <img
+            className='aspect-square object-cover rounded-full border shadow-lg cursor-pointer h-full w-full max-h-[3.5rem]'
+            src={src}
+            onClick={handleOnClick}
+          />
+        ) : (
+          <IoPersonCircleSharp
+            className='text-gray-300 cursor-pointer w-full h-[3.5rem]'
+            onClick={handleOnClick}
+          />
+        )}
       </Skeleton>
     </div>
   )
